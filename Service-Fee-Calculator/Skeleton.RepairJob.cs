87a8f984b548447bd004654314e12b2ccc99301b
mohammedsayed404 <mmossyed525@gmail.com>
2024-07-09 20:02:33 +0300

    Simple-Service-Fee-Calculator

---
+++ Service-Fee-Calculator/Skeleton.RepairJob.cs
@@ -0,0 +1,67 @@
+﻿namespace Service_Fee_Calculator;
+
+
+public abstract class RepairJob
+{
+    public string Description { get; set; } = "";
+
+    public DateTime Start { get; set; }
+
+    public DateTime End { get; set; }
+
+    public bool Successful { get; set; }
+
+    protected double Duration => (End - Start).TotalHours;
+    public abstract decimal CalculateFee();
+
+}
+
+
+
+
+public abstract class TeamRepairJob : RepairJob
+{
+    public int NumberOfMechanics { get; set; } = 1;
+
+    public override decimal CalculateFee() => CalculateFeeSingleMechanic() * NumberOfMechanics;
+    
+    public abstract decimal CalculateFeeSingleMechanic();
+}
+
+//? BasicJob
+
+public class BasicRepairJob : TeamRepairJob
+{
+
+    public override decimal CalculateFeeSingleMechanic()
+    {
+        var quarters = (int)Math.Ceiling(Duration * 4);
+
+        return quarters * 5;
+    }
+}
+
+//! Regular Job 
+public class RegularRepairJob : TeamRepairJob
+{
+
+    public override decimal CalculateFeeSingleMechanic()
+    {
+        return (int)Math.Ceiling(Duration) * 80;
+    }
+}
+
+//todo ComplexJob
+
+public class ComplexRepairJob : RepairJob
+{
+    public override decimal CalculateFee()
+    {
+        if (Duration <= 4)
+        {
+            return 500;
+        }
+
+        return 800;
+    }
+}
---
+++ ServiceFeeCalculatorApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using Service_Fee_Calculator;
+Console.Write("How May jobs you need :");
+var NumberOfJob = int.Parse(Console.ReadLine()!);
+
+List<RepairJob> jobs = new (NumberOfJob);
+int jobsNumber = 1;
+while (NumberOfJob -- > 0)
+{
+
+Console.Write($"Type of repair job {jobsNumber++} :(basic, regular, complex):");
+var type = Console.ReadLine() ?? "";
+Console.Write("Description: ");
+var description = Console.ReadLine() ?? "";
+Console.Write("Start date and time (yyyy-MM-ddTHH:mm:ss): ");
+var start = DateTime.Parse(Console.ReadLine()! );
+Console.Write("End date and time (yyyy-MM-ddTHH:mm:ss): ");
+var end = DateTime.Parse(Console.ReadLine()! );
+RepairJob job = type switch 
+{
+    "basic" => new BasicRepairJob(),
+    "regular" => new RegularRepairJob(),
+    "complex" => new ComplexRepairJob(),
+    _ => throw new ArgumentException("Invalid type of repair job.")
+    
+};
+
+job.Description = description;
+job.Start = start;
+job.End = end;
+jobs.Add(job);
+
+}
+
+foreach (var job in jobs)
+{
+    Console.WriteLine($"Fee: {job.CalculateFee():C}");
+}
+
+//2023-09-28T09:00:00
